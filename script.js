86696ab72cfc4d47d7b732fb7d371f0b745a1d5b
udinocera <udinocera@gmail.com>
2023-08-12 00:31:16 +0200

    esercizio

---
+++ script.js
@@ -0,0 +1,21 @@
+function sortPeopleByAge(arr) {
+    const sortingByAge = people.sort((a, b) => a.age - b.age);
+    return sortingByAge;
+  
+}
+
+const people = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const sortingByAge = sortPeopleByAge(people)
+console.log(sortingByAge)
